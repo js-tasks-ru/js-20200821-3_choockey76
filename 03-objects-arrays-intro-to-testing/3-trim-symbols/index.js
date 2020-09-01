748e5e2298961a87271b1903306c32c4abf16270
OBeliakov <36472401+OBeliakov@users.noreply.github.com>
2020-09-01 13:11:35 +0300

    trimSymbols and uniq completed

--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -5,5 +5,30 @@
  * @returns {string} - the new string without extra symbols according passed size
  */
 export function trimSymbols(string, size) {
+  let includesCount = 0;
+  let formattedString = '';
 
+  if (size === 0) {
+    return ''
+  }
+
+
+  if (!size) {
+    return string;
+  }
+
+  
+  for (let i=0; i < string.length; i++) {
+    if (string[i] === string[i+1]) {
+      includesCount++;
+    } else {
+      includesCount = 0;
+    }
+
+    if (includesCount < size) {
+      formattedString+=string[i];
+    }
+  }
+
+  return formattedString
 }
